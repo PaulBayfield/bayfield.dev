e5fd707a2e5b928a4779322db3f7101c4f66e9ee
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2023-12-19 08:49:13 +0100

    Added a few new internal routes to log all 404 errors

--- website/routes/internal/internal.py
+++ website/routes/internal/internal.py
@@ -11,6 +11,31 @@ def init(app):
     )
 
 
+    @blueprint.api_endpoint(app, uri="/<path:path>", method=['GET','POST'], log_file="/logging/website.log")
+    async def catch_all(path):
+        return Respond.invalid(key="cause", data=f"Route {path} not found!", success=False)
+    
+
+    @blueprint.api_endpoint(app, uri="/<path:path>", method=['GET','POST'], subdomain="saintthibault", log_file="/logging/website.log")
+    async def catch_all_saintthibault(path):
+        return Respond.invalid(key="cause", data=f"Route {path} not found!", success=False)
+
+
+    @blueprint.api_endpoint(app, uri="/<path:path>", method=['GET','POST'], subdomain="youtube", log_file="/logging/website.log")
+    async def catch_all_youtube(path):
+        return Respond.invalid(key="cause", data=f"Route {path} not found!", success=False)
+
+
+    @blueprint.api_endpoint(app, uri="/<path:path>", method=['GET','POST'], subdomain="map", log_file="/logging/website.log")
+    async def catch_all_map(path):
+        return Respond.invalid(key="cause", data=f"Route {path} not found!", success=False)
+
+
+    @blueprint.api_endpoint(app, uri="/<path:path>", method=['GET','POST'], subdomain="media", log_file="/logging/website.log")
+    async def catch_all_media(path):
+        return Respond.invalid(key="cause", data=f"Route {path} not found!", success=False)
+
+
     @blueprint.path(app, uri='/favicon.ico', method=['GET','POST'], log_file="logging/website.log")
     async def home_favicon():
         """
@@ -43,6 +68,16 @@ def init(app):
 
     @blueprint.path(app, uri='/favicon.ico', method=['GET','POST'], subdomain="map", log_file="logging/website.log")
     async def map_favicon():
+        """
+        The favicon of the website.
+
+        :return: The favicon.
+        """
+        return await send_from_directory(f"{app.path}/website/static", 'favicon.ico', mimetype='image/x-icon')
+
+
+    @blueprint.path(app, uri='/favicon.ico', method=['GET','POST'], subdomain="media", log_file="logging/website.log")
+    async def media_favicon():
         """
         The favicon of the website.
 
